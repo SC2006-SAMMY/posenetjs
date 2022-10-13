60000b8d434e0f71b4ca9a6ce40e63f2c455e1d8
yufei <yufei.sg@gmail.com>
2022-10-13 22:31:20 +0800

    Updates

---
+++ precache-manifest.c62207cb034a90631b56f8ae98f1a7c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5723be035caa08536e54c814c3fa8294",
+    "url": "/SC2006-SAMMY/posenetjs/index.html"
+  },
+  {
+    "revision": "29cbf3b7e91624ccd729",
+    "url": "/SC2006-SAMMY/posenetjs/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "b31754d5b2509ed8ab1a",
+    "url": "/SC2006-SAMMY/posenetjs/static/js/2.2771482a.chunk.js"
+  },
+  {
+    "revision": "7dc2529ea9c355027961799bc1ffe0c3",
+    "url": "/SC2006-SAMMY/posenetjs/static/js/2.2771482a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29cbf3b7e91624ccd729",
+    "url": "/SC2006-SAMMY/posenetjs/static/js/main.40b20501.chunk.js"
+  },
+  {
+    "revision": "3e42efaf928aab9b90a5",
+    "url": "/SC2006-SAMMY/posenetjs/static/js/runtime-main.f82d7b20.js"
+  }
+]);
